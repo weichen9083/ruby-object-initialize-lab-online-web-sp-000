26f8a6310f5e08d71d3f92f4a978f02d8e3ad00b
wei chen <weichen9083@gmail.com>
2019-09-26 03:12:39 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog 
+  
+  def initialize(name,breed = "Mutt")
+    @name = name 
+    @breed = breed 
+  end 
+  
+  attr_accessor:name , :breed
+    
+end 
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+
+class Person 
+  
+  
+ def initialize(name)
+    @name = name 
+  end 
+
+
+def name=(what)
+  @name = what 
+end 
+
+
+def name 
+  @name 
+end 
+
+end 
